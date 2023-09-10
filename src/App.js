7c63f7b319f7fb9931b5130a4469a948e5e1233d
Abinandan <abinandannaik@gmail.com>
2023-09-10 22:07:29 +0800

    register page updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import './App.css';
-import { useState } from 'react';
-import Axios from 'axios';
+import Register from './Register';
 
 function App() {
-  const [excuseRes, setExcuseRes] = useState(null);
-
-  const fetchData = (category) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}/`).then((res) => {
-      setExcuseRes(res.data[0]);
-    });
-    
-  };
 
   return (
-    <div className="App">
-      <h1>Click to print execuses based on the Reasons</h1>
-      <button onClick={() => {fetchData("party")}}>Party</button>
-      <button onClick={() => {fetchData("family")}}>Family</button>
-      <button onClick={() => {fetchData("office")}}>Office</button>
-      <button onClick={() => {fetchData("children")}}>Children</button>
-      <h1 style={{color:'green'}}> Excuse: {excuseRes?.excuse}</h1>
-    </div>
+    <main className="App">
+      <Register />
+    </main>
   );
 }
 
